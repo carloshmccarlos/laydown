b5419b73c712489ef4f199756358672f9c795af1
carloshmccarlos <carloshmccarlos@gmail.com>
2026-05-18 13:57:31 +0800

    Add Google Analytics gtag (G-Q7Z5VRDG62)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -116,6 +116,15 @@ export default function RootLayout({
   return (
     <html lang="zh-CN">
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q7Z5VRDG62" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-Q7Z5VRDG62');`,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
